dbabf1597a7777b6cadcb2676407fe3ea02228c0
Helmi Zulfikar <helmi.zulfikar20@gmail.com>
2018-09-19 09:33:30 +0700

    fix 1

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const config = {
     Interval : 1, //in Seconds
-    DiskMonitoring : ['/dev/sdb5', '/dev/loop22', '/dev/loop24'], //array
+    DiskMonitoring : ['/dev/sdb5'], //array
     AlertRAM : 70, //persent use
     AlertStorage : 70, //persent use
     SlackWebhookURL : 'https://hooks.slack.com/services/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'
